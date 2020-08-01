f9af927d8ceea62639de30b744a689fa9fcbe061
HakkiSabanci <hakkisabanci@yahoo.com>
2020-08-01 12:52:10 -0400

    update3 HappyAgain.Java

--- src/HappyAgain.java
+++ src/HappyAgain.java
@@ -3,7 +3,7 @@ public class HappyAgain {
         System.out.println("java us is the bessttt");
         System.out.println("NEsibe IS the Best");
         int a =10;
-        int b= 3;
+        int b= 5;
         int c=a+b;
         System.out.println(c);
     }
